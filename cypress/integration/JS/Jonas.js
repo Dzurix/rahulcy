ee4784706d3bfecb9dc7737ae9ba85bbcb8a76a9
Dejan Stanišin <dsta@novalite.rs>
2022-05-18 15:48:01 +0200

    chalenge 6

--- cypress/integration/JS/Jonas.js
+++ cypress/integration/JS/Jonas.js
@@ -3,6 +3,8 @@
 // console.log(inputYear + 18); // rezultat je 199118
 // console.log(Number(inputYear) + 18); // rezultat je 2009
 
+const { func } = require("assert-plus");
+
 // console.log(Number("Jonas")); // rezultat je NaN - Not a Number (invalid number)
 
 // console.log(String(23), 23); // druga boja u consoli nam govori da je rec o stringu
@@ -101,43 +103,183 @@
 //   console.log("There are no winners 🚫");
 // }
 
-const day = "monday";
+// const day = "monday";
 
-switch (day) {
-  case "monday": // day === 'monday'
-    console.log("Plan course structure");
-    console.log("Go to coding meetup");
-    break; // bez break samo bi nastavio da izvrsava ostali kod
+// switch (day) {
+//   case "monday": // day === 'monday'
+//     console.log("Plan course structure");
+//     console.log("Go to coding meetup");
+//     break; // bez break samo bi nastavio da izvrsava ostali kod
 
-  case "tuesday":
-    console.log("Prepare theory videos");
-    break;
-  case "wednesday":
-  case "thursday":
-    console.log("Write code examples");
-    break;
-  case "friday":
-    console.log("Records videos");
-    break;
-  case "saturday":
-  case "sunday":
-    console.log("Enjoy the weekend :D ");
-    break;
-  default:
-    console.log("Not a valid day!");
-}
+//   case "tuesday":
+//     console.log("Prepare theory videos");
+//     break;
+//   case "wednesday":
+//   case "thursday":
+//     console.log("Write code examples");
+//     break;
+//   case "friday":
+//     console.log("Records videos");
+//     break;
+//   case "saturday":
+//   case "sunday":
+//     console.log("Enjoy the weekend :D ");
+//     break;
+//   default:
+//     console.log("Not a valid day!");
+// }
 
-if (day === "monday") {
-  console.log("Plan course structure");
-  console.log("Go to coding meetup");
-} else if (day === "tuesday") {
-  console.log("Prepare theory videos");
-} else if (day === "wednesday" || day === "thursday") {
-  console.log("Write code examples");
-} else if (day === "friday") {
-  console.log("Records videos");
-} else if (day === "saturday" || day === "sunday") {
-  console.log("Enjoy the weekend :D ");
-} else {
-  console.log("Not a valid day!");
-}
+// if (day === "monday") {
+//   console.log("Plan course structure");
+//   console.log("Go to coding meetup");
+// } else if (day === "tuesday") {
+//   console.log("Prepare theory videos");
+// } else if (day === "wednesday" || day === "thursday") {
+//   console.log("Write code examples");
+// } else if (day === "friday") {
+//   console.log("Records videos");
+// } else if (day === "saturday" || day === "sunday") {
+//   console.log("Enjoy the weekend :D ");
+// } else {
+//   console.log("Not a valid day!");
+// }
+
+// const appleJuice = fruitProcesor(5, 0);
+// console.log(fruitProcesor(5, 0));
+// //FUNCTION DECLARATION
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
+// const age1 = calcAge1(1991);
+
+// //FUNCTION EXPRESSION  - kada funkciju smestimo u varijablu (samim tim ce ta varijabla postati funkcija)
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const age2 = calcAge2(1991);
+// console.log(age1, age2);
+
+// // Arrow function
+
+// const calcAge3 = (birthYear) => 2037 - birthYear;
+
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   // ako ima vise parameratara, onda mora zagrada ()
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   // return retirement; // ako ima vise linija koda, onda ipak moramo da pozovemo "return"
+//   return `${firstName} retires in ${retirement} years.`;
+// };
+// console.log(yearsUntilRetirement(1991, "Jonas"));
+
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+
+// function fruitProcesor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+//   const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of orange.`;
+//   return juice;
+// }
+
+// fruitProcesor(2, 3);
+
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years.`);
+//     return retirement; // posle return , funkcija prestaje za izvrsavanjem - SVE POSLE SE ignorise
+//   } else {
+//     console.log(`${firstName} has already retired`);
+//     return -1;
+//   }
+// };
+// console.log(yearsUntilRetirement(1991, "Jonas"));
+// console.log(yearsUntilRetirement(1970, "Mike"));
+
+//coding chalenge 5
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+let avgDolphins = calcAverage(44, 23, 71);
+let avgKoalas = calcAverage(65, 54, 49);
+
+console.log(avgDolphins, avgKoalas);
+
+//checkWinner
+
+// const checkWinner = () => {
+//   if (avgDolphins > avgKoalas * 2) {
+//     console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+//   } else if (avgKoalas > avgDolphins * 2) {
+//     console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+//   } else {
+//     console.log(`No teams win`);
+//   }
+// };
+
+// console.log(checkWinner());
+
+// const friends = ["Michael", "Steven", "Peter"]; // literal sintax
+// const years = new Array(1991, 1984, 2000);
+
+// console.log(friends[0]);
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
+
+// friends[2] = "Jay"; // dodeljivanje novog clana nizu iako je tip CONST -
+// // ovo moze samo zato sto primitivne vrednosti se ne mogu menjati a ARRAY to nije
+// // ono sto ne moze to je da zamenimo ceo ARRAY ako smo vec definisali kao CONST
+// console.log(friends);
+
+// const jonas = [firstname, "Schmedtmann", 2037 - 1991, "teacher", friends];
+// // mozemo koristiti razne vrste podataka u Array
+
+//Exercise
+// const friends = ["Michael", "Steven", "Peter"];
+// const newLenght = friends.push("Jay"); // metod koji dodaje novi clan na kraju niza
+// //push je metod, tj. funkcija koja vraca vrednost, u ovom slucaju velicinu Array - to dobijemo
+// //kada je uskladistimo u novu varijablu
+// friends.unshift("John"); // metod koji dodaje novi clan na pocetku niza
+// console.log(friends);
+
+// friends.pop(); //brise poslednji element
+// friends.shift(); // brise prvi element
+
+// console.log(friends.indexOf("Steven")); // pokazuje na kojoj je poziciji element niza
+
+// if (friends.includes("Steven")) {
+//   console.log(" You have a friend called Steven");
+// } // vraca true ili false ako postoji taj element u nizu
+
+//coding chalenge 6
+
+const calcTip = (bill) => {
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.2;
+  }
+};
+
+console.log(calcTip(301));
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(tips);
+console.log(total);
