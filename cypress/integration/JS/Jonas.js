19a2a86ae630c83a3dd803cc73ef5f2eab77b914
Dejan Stanišin <dsta@novalite.rs>
2022-05-18 08:18:25 +0200

    izmena

--- cypress/integration/JS/Jonas.js
+++ cypress/integration/JS/Jonas.js
@@ -114,16 +114,32 @@ switch (day) {
   case "tuesday":
     console.log("Prepare theory videos");
     break;
-  case "wendesday":
+  case "wednesday":
   case "thursday":
     console.log("Write code examples");
     break;
   case "friday":
     console.log("Records videos");
+    break;
   case "saturday":
   case "sunday":
     console.log("Enjoy the weekend :D ");
     break;
   default:
     console.log("Not a valid day!");
+}
+
+if (day === "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Records videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy the weekend :D ");
+} else {
+  console.log("Not a valid day!");
 }
