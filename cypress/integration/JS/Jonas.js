ab4f34155c9789705ee316c6a88531f17c94aed0
Dejan Stanišin <deki2ns@gmail.com>
2022-05-19 18:56:27 +0200

    izmene od kuce

--- cypress/integration/JS/Jonas.js
+++ cypress/integration/JS/Jonas.js
@@ -1,9 +1,91 @@
+"use strict";
+
+//let firstname = "upoje";
+
+//console.log(firstname);
+/*
+console.log("glisoje");
+console.log('radoje');
+console.log('blagoje');
+*/
+
+//coding chalenge 1
+
+// let marksHight1 = 1.69;
+// let marksWeight1 = 78;
+// let marksWeight2 = 95;
+// let marksHight2 = 1.88;
+
+// let johnsHeight1 = 1.95;
+// let johnsWeight1 = 92;
+// let johnsHeight2 = 1.76;
+// let johnsWeight2 = 85;
+
+// let marksBMI1 = marksWeight1 / marksHight1 ** 2;
+// console.log("Marks BMI 1 is:", marksBMI1);
+
+// let johnsBMI1 = johnsWeight1 / johnsHeight1 ** 2;
+// console.log("Johns BMI is", johnsBMI1);
+
+// let marksBMI2 = marksWeight2 / marksHight2 ** 2;
+// console.log("Marks BMI 2 is;", marksBMI2);
+
+// let johnsBMI2 = johnsWeight2 / johnsHeight2 ** 2;
+// console.log("Johns BMI 2 is ", johnsBMI2);
+
+// let marksHigherBMI1 = marksBMI1 > johnsBMI1;
+// console.log(marksHigherBMI1);
+
+// let marksHigherBMI2 = marksBMI2 > johnsBMI2;
+// console.log(marksHigherBMI2);
+
+// console.log("coding chalenge 2");
+
+// if (marksBMI1 > johnsBMI1) {
+//   console.log(`Marks BMI ${marksBMI1} is greather then Johns ${johnsBMI1}`);
+// } else {
+//   console.log(`Johns BMI ${johnsBMI1} is greather then Marks BMI ${marksBMI1}`);
+// }
+
+// const firstname = "Jonas";
+// const job = "teacher";
+// const birthYear = 1991;
+
+// const jonas =
+//   // "I am " + firstname + " ,a " + (2037 - birthYear) + " years old " + job; OVAKO NE TREBA NIKADDDDD
+
+//   //vec ovako treba
+//   `I'm ${firstname}, a ${2037 - birthYear} years old ${job} !!!`;
+
+// console.log(jonas);
+
+// console.log(`Kreiranje
+// stringa koji
+// prelazi u
+// vise linija. Ovo
+// je mnogo
+// lakse
+// za
+// pisanje`);
+
+// const age = 10;
+// const isOldEnought = age >= 18;
+
+// if (isOldEnought) {
+//   console.log(`Sarah can start driving lessons 🚘`);
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`Sarah is too young. Wait another ${yearsLeft} years 🤞💕`);
+// }
+
+// coding chalenge 2
+
 // const inputYear = "1991";
 // console.log(Number(inputYear));
 // console.log(inputYear + 18); // rezultat je 199118
 // console.log(Number(inputYear) + 18); // rezultat je 2009
 
-const { min } = require("lodash");
+//const { min } = require("lodash");
 
 // console.log(Number("Jonas")); // rezultat je NaN - Not a Number (invalid number)
 
@@ -103,27 +185,67 @@ const { min } = require("lodash");
 //   console.log("There are no winners 🚫");
 // }
 
-const day = "monday";
+// const day = "monday";
 
-switch (day) {
-  case "monday": // day === 'monday'
-    console.log("Plan course structure");
-    console.log("Go to coding meetup");
-    break; // bez break samo bi nastavio da izvrsava ostali kod
+// switch (day) {
+//   case "monday": // day === 'monday'
+//     console.log("Plan course structure");
+//     console.log("Go to coding meetup");
+//     break; // bez break samo bi nastavio da izvrsava ostali kod
 
-  case "tuesday":
-    console.log("Prepare theory videos");
-    break;
-  case "wendesday":
-  case "thursday":
-    console.log("Write code examples");
-    break;
-  case "friday":
-    console.log("Records videos");
-  case "saturday":
-  case "sunday":
-    console.log("Enjoy the weekend :D ");
-    break;
-  default:
-    console.log("Not a valid day!");
+//   case "tuesday":
+//     console.log("Prepare theory videos");
+//     break;
+//   case "wendesday":
+//   case "thursday":
+//     console.log("Write code examples");
+//     break;
+//   case "friday":
+//     console.log("Records videos");
+//   case "saturday":
+//   case "sunday":
+//     console.log("Enjoy the weekend :D ");
+//     break;
+//   default:
+//     console.log("Not a valid day!");
+// }
+
+// const age = 23;
+
+// age >= 18
+//   ? console.log("I like to drink wine")
+//   : console.log("I like to drink water");
+
+// const drink = age >= 18 ? "wine" : "water";
+
+// console.log(drink);
+
+// console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+
+// CODING CHALENGE 4
+
+// let bill = 10;
+// let tip = bill > 50 && bill < 300 ? bill * 0.15 : bill * 0.2;
+
+// console.log(
+//   `The bill was ${bill}, the tip was ${tip} and the total value is ${
+//     bill + tip
+//   }...`
+// );
+
+// function logger() {
+//   console.log("My name is Upoje");
+// }
+
+// logger(); // invoking, calling, running the function
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+
+  return juice;
 }
+
+fruitProcessor(5, 0);
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
