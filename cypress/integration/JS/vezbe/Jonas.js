45a7e66640d0b7c59e7ede66f404e4fea64e395f
Dejan Stanišin <dsta@novalite.rs>
2022-06-07 10:24:36 +0200

    izmene

--- cypress/integration/JS/vezbe/Jonas.js
+++ cypress/integration/JS/vezbe/Jonas.js
@@ -1392,43 +1392,24 @@ const game = {
   },
 };
 
-//Create one player array for each team (variables 'players1' and 'players2')
+//1
+const [players1, players2] = game.players;
 
-const players1 = game.players[0];
-const players2 = game.players[1];
-
-//The first player in any player array is the goalkeeper and the others are field
-// players. For Bayern Munich (team 1) create one variable ('gk') with the
-// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
-// field players
-
+//2
 const [gk, ...fieldPlayers] = players1;
 
-//Create an array 'allPlayers' containing all players of both teams (22
-//players)
-
+//3
 const allPlayers = [...players1, ...players2];
 
-//During the game, Bayern Munich (team 1) used 3 substitute players. So create a
-//new array ('players1Final') containing all the original team1 players plus
-//'Thiago', 'Coutinho' and 'Perisic'
-
+//4
 const playersFinal = [...players1, "Thiago", "Coutinho", "Perisic"];
 
-//Based on the game.odds object, create one variable for each odd (called
-//'team1', 'draw' and 'team2')
+//5
+const { team1, x: draw, team2 } = game.odds;
 
-const { team1, x, team2 } = game.odds;
-
-//Write a function ('printGoals') that receives an arbitrary number of player
-//names (not an array) and prints each of them to the console, along with the
-//number of goals that were scored in total (number of player names passed in)
-
+//6
 console.log(game.printGoals("Davies", "Muller", "Lewandowski"));
 console.log(game.printGoals(game.scored));
 
-//The team with the lower odd is more likely to win. Print to the console which
-//team is more likely to win, without using an if/else statement or the ternary
-// operator.
-
-console.log(team1 || team2 || x);
+//7
+team1 < team2 && console.log("Team 1 is more likely to win");
