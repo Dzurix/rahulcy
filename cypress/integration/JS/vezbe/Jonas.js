ef0ad09abc7a0afe654197694265bca792dc2797
Dejan Stanišin <dsta@novalite.rs>
2022-06-14 08:46:31 +0200

    practice Strings

--- cypress/integration/JS/vezbe/Jonas.js
+++ cypress/integration/JS/vezbe/Jonas.js
@@ -1925,13 +1925,21 @@ const changeCase = function (word) {
   const newCase = [i + z[0].toUpperCase() + z.slice(1)].join();
   console.log(`${newCase.padEnd(25, ' ')}` + `${'✅'.repeat()}`);
 };
-
+//string methods practice
 changeCase('  calculate_AGE');  */
+
+const getCode = str => str.slice(0, 3).toUpperCase();
 
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 for (const flight of flights.split('+')) {
   const [type, from, to, time] = flight.split(';');
-  const output = `${type} ${from} ${to} ${time}`;
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(
+    45
+  );
+  console.log(output);
 }
