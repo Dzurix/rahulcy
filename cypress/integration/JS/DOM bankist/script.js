8d6e76e7b22549d6449b783836ddc62c81080083
Dejan Stanišin <dsta@novalite.rs>
2022-07-14 13:18:51 +0200

    selecting elements

--- cypress/integration/JS/DOM bankist/script.js
+++ cypress/integration/JS/DOM bankist/script.js
@@ -33,3 +33,74 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+////////////////////////////////////////////////////////////////
+//          D   O   M      //
+///////////////////////////////////////////////////////////////
+
+//Selecting elements
+
+console.log(document.documentElement); //selektovanje celog DOCUMENTa
+console.log(document.head); //selektovanje HEAD-a
+console.log(document.body); //selektovanje BODY
+
+const header = document.querySelector('.header'); //hvatanje prvog elementa koji odgovara 'header'
+const allSections = document.querySelectorAll('.section'); // hvatanje vise elemenata sa clasom SECTION
+console.log(allSections); // vraca NODE LIST
+
+document.getElementById('section--1'); // bez #, to je samo za query-je
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons); // ovo vraca COLLECTIONS koje se automatski update-uju
+
+document.getElementsByClassName('btn');
+
+//Creating and inserting elements
+
+//  mozemo preko  .insertAdjacentHTML
+
+//a drugi nacin je ovako
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent = 'We use cookies for improved functionality and analytics';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class = "btn btn--close-cookie">Got it!</button>';
+
+//header.prepend(message); // dodavanje poruke pre svih - da bude prvi element
+header.append(message); // dodavanje poruke da bude poslednja
+//header.append(message.cloneNode(true)); // ovako kopiramo, pa mozemo videti i kao prepend i kao apend
+
+//header.before(message); //pre hedera
+// header.after(message) // posle hedera
+
+// Delete elements
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    //message.remove(); // novi nacin brisanja
+
+    // stari nacin brisanja
+    message.parentElement.removeChild(message);
+  });
+
+//Styles
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.backgroundColor); // vidimo zato smo smo mi definisali rucno
+console.log(message.style.color); // ne dobijamo nista, iako je u CSS
+console.log(getComputedStyle(message).color); //ovako dobijamo color iz CSS
+console.log(getComputedStyle(message).height); //ovako dobijamo height koja je prikazana na stranici
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px'; //menamo visinu
+
+// CSS varijable
+
+// root = DOCUMENT
+
+document.documentElement.style.setProperty('--color-primary', 'orangered'); // setovanje propertyja
+
+// Atributes
