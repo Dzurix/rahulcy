5788b4cbbe6a5d2f311bcd30c5760086adc234cc
Dejan Stanišin <dsta@novalite.rs>
2022-07-18 16:02:46 +0200

    revealing elements on scroll

--- cypress/integration/JS/DOM bankist/script.js
+++ cypress/integration/JS/DOM bankist/script.js
@@ -9,6 +9,10 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -264,3 +268,177 @@ document.querySelector('.nav').addEventListener('click', function (e) {
 
 
 */
+
+//DOM TRAVERSING
+
+const h1 = document.querySelector('h1');
+
+// Going downwards -  child
+console.log(h1.querySelectorAll('.highlight'));
+//querySelectorAll selektuje sve elemente sa 'highlight' klasom koja su deca od 'h1', bez obzira koliko duboko se nalaze
+
+console.log(h1.childNodes); // direktna deca od 'h1'
+// NODES mogu biti bilo sta (text, comment,...)
+console.log(h1.children); //dobijemo HTML kolekciju tj. elementi koji su unutar 'h1'
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+//Going upwords - selecting parents
+
+console.log(h1.parentNode); // - direktni roditelj
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)'; //trazenje najblizeg roditelja od 'h1' sa klasom '.header'
+h1.closest('h1').style.background = 'var(--gradient-primary)'; // menjanje stila na samom elementu 'h1'
+
+// CLOSEST metod je ustvari suprotno od querySelector (koji trazi decu, bez obzira koliko duboko u DOM) posto trazi roditelje, bez
+// obzira koliko visoko na DOM
+
+// Going sideways - braca - U JS mozemo selektovati samo direktnu bracu (prethodni i sledeci)
+
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+//ako nam trebaju sva braca
+//oticu u roditelja i pogledati svu decu
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
+
+//Building Tabbed Component
+// sve se zasniva na dodavanju i brisanju klasa (kao kod 'Modal' windowa)
+
+// tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
+// ovo necemo ovako selektovati, jer sta npr da ima hiljadu elemenata, sve bi ih selektovali
+//vec cemo selektovati samo one za koje smo zainteresovani preko "DELEGATION", na zajednicom roditelju
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab'); // hvatamo samo tabove, tj button
+  console.log(clicked);
+  if (!clicked) return; //  ako nije kliknuto odmah se vrati
+  // ovo je GUARD CLAUSE - if koji se vraca rano ako je neki uslov zadovoljen
+  // i sve posle toga se nece izvrsiti
+
+  //Active tab
+  tabs.forEach(t => t.classList.remove('operations__tab--active')); //prvo oduzmemo klasu svima
+  clicked.classList.add('operations__tab--active'); // pa onda dodelimo samo na koju se klikne
+
+  //remove active clasess
+
+  tabsContent.forEach(c => c.classList.remove('operations__content--active')); //prvo oduzmemo klasu
+
+  //Activate content area
+  console.log(clicked.dataset.tab);
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active'); // pa dodajemo klasu
+});
+
+// HOW TO PASS ARGUMENTS to EVENT HANDLERS
+// menu fade animation
+
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains('nav__link')) {
+    //ovde ne koristimo closest() metod zato sto nema CHILD elemenata
+    //koje bi slucajno mogli da kliknemo
+
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      // if (el !== link) el.style.opacity = opacity //ovako ako idem preko kreiranje funkcije handleHover
+      if (el !== link) el.style.opacity = this; //ovako ako koristim BIND metod za pozivanje funkcije
+    });
+    //logo.style.opacity = opacity
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener(
+  'mouseover',
+  handleHover.bind(0.5)
+  //e => handleHover(e, 0.5)
+  // moramo ovako ubaciti funkciju, jer addEventListener ocekuje funkciju kao drugi element
+);
+//na primer,mozemo napisati na drugi nacin preko bind
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+//IMPLEMENTING a STICKY NAVIGATION - The Scroll Event
+
+//Sticky navigation
+
+//odredjivanje pozicije section 1
+
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener('scroll', function () {
+  console.log(this.window.scrollY);
+
+  if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+
+//Sticky navigation preko INTERSECTION OBSERVER API
+
+// const obsCallback = function (entries, observer) {
+//   //entries su ARRAY of threshold iz obsOptions
+//   // obsCallback funkcija ce biti pozvana svaki put kada posmatrani element (target element (section1))
+//   // intersectira root element na threshold koji definisemo
+
+//   entries.forEach(entry => console.log(entry));
+// };
+// const obsOptions = {
+//   root: null, //element koji target intersectira //  kad je null onda je viewport tj, ono sto vidimo na ekranu
+//   threshold: [0, 0.2], //procenat intersectiona na kojem observer callback ce biti pozvan
+//   // (procenat koji hocemo da imamo vidljiv u nazem root)
+// };
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector('.header');
+const navHeigt = nav.getBoundingClientRect().height;
+console.log(navHeigt);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0, //ako je 0% headera vidljivo, onda hocemo nesto...
+  rootMargin: `-${navHeigt}px`, // odakle krece (zavrsava) header
+});
+headerObserver.observe(header);
+
+//REVEALING ELEMENTS on SCROLL
+
+//reveal section
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return; //   GUARD CLAUSE
+  entry.target.classList.remove('section--hidden');
+
+  observer.unobserve(entry.target); // iskljucujemo observer - JAKO VAZNO da isklkjucimo zbog performansi
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
