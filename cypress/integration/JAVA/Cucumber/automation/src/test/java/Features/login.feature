ced3f73d5d68f147a0fbfde8f53258e553fa294f
Dejan Stanišin <dsta@novalite.rs>
2022-07-20 14:34:48 +0200

    setings JSON

--- cypress/integration/JAVA/Cucumber/automation/src/test/java/Features/login.feature
+++ cypress/integration/JAVA/Cucumber/automation/src/test/java/Features/login.feature
@@ -1,9 +1,8 @@
 Feature: Application login
 
-
 Scenario: Home page default login
 
 Given User is on NetBanking landing page
-When user login into application with username and password
-Then home page is populated
-And Cards are displayed
+When User login into application with username and password
+Then Home page is populated
+And Cards are displayed 
--- cypress/integration/JAVA/Cucumber/automation/src/test/java/stepDefinitions/stepDefinition.java
+++ cypress/integration/JAVA/Cucumber/automation/src/test/java/stepDefinitions/stepDefinition.java
@@ -10,28 +10,28 @@ import io.cucumber.java.en.When;
 
 public class stepDefinition {
     
-    
+  
     @Given("^User is on NetBanking landing page$")
-    public void user_is_on_netbanking_landing_page() throws Throwable {
-       //some code
+    public void user_is_on_netbanking_landing_page() {
+        //code
+
     }
-   
 
-
-    @When("^user login into application with username and password$")
+    @When("^User login into application with username and password$")
     public void user_login_into_application_with_username_and_password() throws Throwable {
         //code
+
     }
 
-    @Then("^home page is populated$")
+    @Then("^Home page is populated$")
     public void home_page_is_populated() throws Throwable {
+        //code
+
+    }
+
+    @And("^Cards are displayed $")
+    public void cards_are_displayed() throws Throwable {
        //code
     }
-
-    @And("^Cards are displayed$")
-    public void cards_are_displayed() throws Throwable {
-        //code
-    }
-  
 
 }
